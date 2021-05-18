f5b9c555eaa26cc61668bdbe8d6ba35c35bcc2bd
Victor Vo <79146455+victorkvo@users.noreply.github.com>
2021-05-18 14:00:03 -0500

    Initial commit

---
+++ src/test/java/com/tts4/_oauth2demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts4._oauth2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
